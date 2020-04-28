094e4c0586549c44e138440549c98ce39790750f
Chris Lim <cli333@protonmail.com>
2020-04-27 20:23:23 -0700

    a

--- flow/src/App.tsx
+++ flow/src/App.tsx
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const App = (): any => {
-  return <div className="App">HELLO docker!</div>;
+  return <div className="App">HELLO docker!🐳</div>;
 };
 
 export default App;
